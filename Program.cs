4a30e900ea0054d0da2a757de3e5648ce8186ca9
Josh <jb7@fastmail.com>
2019-12-06 10:20:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NAudio.CoreAudioApi;
+
+namespace volumeReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MMDeviceEnumerator denum = new MMDeviceEnumerator();
+            MMDevice defaultDevice = denum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            float level = 0.36f;
+            defaultDevice.AudioEndpointVolume.Channels[0].VolumeLevelScalar = level;
+            defaultDevice.AudioEndpointVolume.Channels[1].VolumeLevelScalar = level;
+            
+        }
+    }
+}
