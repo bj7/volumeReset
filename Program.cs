2edae81895bbd0cac8b2e71fb3bba03eca848993
Josh <jb7@fastmail.com>
2019-12-06 10:34:42 -0500

    Set the balance to current master level

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace volumeReset
             Console.WriteLine("Hello World!");
             MMDeviceEnumerator denum = new MMDeviceEnumerator();
             MMDevice defaultDevice = denum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
-            float level = 0.36f;
-            defaultDevice.AudioEndpointVolume.Channels[0].VolumeLevelScalar = level;
-            defaultDevice.AudioEndpointVolume.Channels[1].VolumeLevelScalar = level;
-            
+            float level0 = defaultDevice.AudioEndpointVolume.Channels[0].VolumeLevelScalar;
+            float level1 = defaultDevice.AudioEndpointVolume.Channels[1].VolumeLevelScalar;
+
+            float masterLevel = level0 > level1 ? level0 : level1;
+            defaultDevice.AudioEndpointVolume.Channels[0].VolumeLevelScalar = masterLevel;
+            defaultDevice.AudioEndpointVolume.Channels[1].VolumeLevelScalar = masterLevel;
         }
     }
 }
